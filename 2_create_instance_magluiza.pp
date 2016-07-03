dd3d5f68d0f25b4d7b7c53cbc7c0d11b5e4dd67f
eazeved0 <eazevedo@bsd.com.br>
2016-07-03 19:43:59 -0300

    Create 2_create_instance_magluiza.pp

---
+++ 2_create_instance_magluiza.pp
@@ -0,0 +1,15 @@
+ec2_instance { 'Mag_Luiza_Instance2':
+  ensure            => present,
+  region            => 'sa-east-1',
+  availability_zone => 'sa-east-1c',
+  subnet	    => 'PRODUCAO-PUBLIC-2',
+  image_id          => 'ami-0fb83963',
+  instance_type     => 't2.medium',
+  monitoring        => false,
+  key_name          => 'training_protonmail',
+  security_groups   => ['EAZEVEDO-INTERNAL'],
+  user_data         => template('/home/ubuntu/Mag_Luiza/script.sh'),
+  tags              => {
+   version => 'v1',
+  },
+}
